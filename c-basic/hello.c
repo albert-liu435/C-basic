76a2aee4c0e5f2c540cdf161897d65b7a2608211
albert.liu435 <albert.liu435@gmail.com>
2023-08-12 22:52:57 +0800

    day01

---
+++ c-basic/hello.c
@@ -0,0 +1,11 @@
+//#:关键字标识符,表示引入头文件 include：引入头文件关键字
+//stdio.h:系统标准输入、输出库对应的头文件，为printf函数服务
+//<>：使用系统库函数。"":用户自定义的库函数
+#include <stdio.h>
+
+int main(void) { //int:表示函数返回值是整形数据。main：函数名，是程序的唯一入口，必须有且只有一次
+
+	printf("hello world\n");
+	//返回当前函数的调用
+	return 0;
+}
